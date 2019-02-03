16db80bb21971dcbcaa3d613a183f3c9fa3a7f49
WadeMadden <wade.madden.15@cnu.edu>
2019-02-03 00:26:10 -0500

    Changed movement to use CharacterController instead of RigidBody. More natural movement

--- MontimerController.cs
+++ MontimerController.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public class MontimerController : MonoBehaviour
 {
-    public Rigidbody mortyRigBod;
+    //public Rigidbody mortyRigBod;
     public float mortySpeed;
     public float jumpSpeed;
+
+    public CharacterController mortyController;
+    private Vector3 mortyDirection;
+    public float gravity;
+
     // Start is called before the first frame update
     void Start()
     {
-        mortyRigBod = GetComponent<Rigidbody>();
+        //mortyRigBod = GetComponent<Rigidbody>();
+
+        mortyController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //enables mobility
+        /*Rigid body made character too floaty
         mortyRigBod.velocity = new Vector3(Input.GetAxis("Horizontal") * mortySpeed, mortyRigBod.velocity.y, Input.GetAxis("Vertical") * mortySpeed);
-        //jumping control
+        
         if (Input.GetButtonDown("Jump"))
         {
             mortyRigBod.velocity = new Vector3(mortyRigBod.velocity.x, jumpSpeed, mortyRigBod.velocity.z);
+        }*/
+
+        //Character controller works better. Character sticks to the ground
+
+        //enables movement
+        mortyDirection = new Vector3(Input.GetAxis("Horizontal") * mortySpeed, 0f, Input.GetAxis("Vertical") * mortySpeed);
+
+        Jump();
+
+        mortyDirection.y = mortyDirection.y + (Physics.gravity.y * gravity);
+        mortyController.Move(mortyDirection * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        //jump check
+        if (mortyController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                mortyDirection.y = jumpSpeed;
+            }
         }
     }
 }
