e74d262c8a5f578aa6ec7768e99301a9a633697a
WadeMadden <wade.madden.15@cnu.edu>
2019-02-03 01:55:45 -0500

    Added extra camera functionality with controller support

--- CameraControl.cs
+++ CameraControl.cs
@@ -8,6 +8,7 @@ public class CameraControl : MonoBehaviour
     public Transform targ;
     public Vector3 offset;
     public bool useOffset;
+    public float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,22 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = targ.position - offset;
+
+        //x position of controller and rotate target
+        float horiz = Input.GetAxis("ControllerHoriz") * rotationSpeed;
+        targ.Rotate(0, horiz, 0);
+
+        float vert = Input.GetAxis("ControllerVert") * rotationSpeed;
+        targ.Rotate(vert, 0f, 0f);
+
+        //change camera based on rotatoin of target and offset
+        float yAng = targ.eulerAngles.y;
+        float xAng = targ.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(xAng, yAng, 0f);
+        transform.position = targ.position - (rotation * offset);
+
+        //transform.position = targ.position - offset;
         transform.LookAt(targ);
     }
 }
