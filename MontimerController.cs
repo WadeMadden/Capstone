6ad66496f9841b546ee281497b1b7f8c411df85a
WadeMadden <wade.madden.15@cnu.edu>
2019-02-02 23:30:39 -0500

    random files added?

--- MontimerController.cs
+++ MontimerController.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MortimerControl : MonoBehaviour
-{   //Initial
+public class MontimerController : MonoBehaviour
+{
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
