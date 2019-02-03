7ecc4c6f1a6765f06dc1bd9f969de12d4b687601
WadeMadden <wade.madden.15@cnu.edu>
2019-02-02 23:54:06 -0500

    Added movement and jump capabilities. Player is too floaty

--- MontimerController.cs
+++ MontimerController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class MontimerController : MonoBehaviour
 {
+    public Rigidbody mortyRigBod;
     public float mortySpeed;
+    public float jumpSpeed;
     // Start is called before the first frame update
     void Start()
     {
-       
+        mortyRigBod = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //enables mobility
+        mortyRigBod.velocity = new Vector3(Input.GetAxis("Horizontal") * mortySpeed, mortyRigBod.velocity.y, Input.GetAxis("Vertical") * mortySpeed);
+        //jumping control
+        if (Input.GetButtonDown("Jump"))
+        {
+            mortyRigBod.velocity = new Vector3(mortyRigBod.velocity.x, jumpSpeed, mortyRigBod.velocity.z);
+        }
     }
 }
