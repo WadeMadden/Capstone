06c7ceb89bf1eb2872f8503fc4aed8e18ac4d2e5
WadeMadden <wade.madden.15@cnu.edu>
2019-02-10 15:35:17 -0500

    Added basic rotation function to gem

--- GemBehaviour.cs
+++ GemBehaviour.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GemBehaviour : MonoBehaviour
 {
+    //allows for turning animation on or off
+    public bool gemAnimated;
+
+    //allows for turning rotation on or off
+    public bool rotating;
+
+    public Vector3 rotateAngle;
+    public float rotateSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,12 @@ public class GemBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gemAnimated)
+        {
+            if (rotating)
+            {
+                transform.Rotate(rotateAngle * rotateSpeed * Time.deltaTime);
+            }
+        }
     }
 }
